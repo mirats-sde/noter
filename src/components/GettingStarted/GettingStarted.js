77b9f819613475837a51f0bc4abed13dd5e9c559
nilesh133 <nkpnilesh13@gmail.com>
2022-04-19 11:58:38 +0530

    netify.toml addition

--- src/components/GettingStarted/GettingStarted.js
+++ src/components/GettingStarted/GettingStarted.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import styles from "./gettingstarted.module.css";
 import Basic from "../Basic/Basic";
 
