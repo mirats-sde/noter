8127e78cc5221919ebdc94e1bfa2c4b89c159ab4
nilesh133 <nkpnilesh13@gmail.com>
2022-04-19 13:01:36 +0530

    uuvid4-removal

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -7,7 +7,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { doc, setDoc, getDocs, query, collection, orderBy, limit } from "firebase/firestore";
-import { uuid } from 'uuidv4';
+import Title from "../Title/Title"
 
 const Editor = () => {
     const doc_id = Date.now();
@@ -31,32 +31,32 @@ const Editor = () => {
     //     )
     // }
     const saveData = async () => {
-       
-       
-            await editor.save().then((outputData) => {
-                console.log('Article data: ', outputData)
-                // generateId().then(async (snapshot) => {
-    
-                //     if(snapshot?.docs[0]){
-                //         doc_id = snapshot.docs[0].data()?.id;
-                //         doc_id = doc_id + 1;
-                //     }
-                //     else{
-                //         doc_id = 1000;
-                //     }
-                // })
-    
-                db.collection("UserData").doc("Users").collection(user.email).doc("editor").collection("userEditor").doc(String(doc_id)).set({
-                    title: editorTitle,
-                    notes: outputData,
-                    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-                    doc_id: doc_id,
-                },
-                    { merge: true })
-            }).catch((error) => {
-                console.log('Saving failed: ', error)
-            });
-        
+
+
+        await editor.save().then((outputData) => {
+            console.log('Article data: ', outputData)
+            // generateId().then(async (snapshot) => {
+
+            //     if(snapshot?.docs[0]){
+            //         doc_id = snapshot.docs[0].data()?.id;
+            //         doc_id = doc_id + 1;
+            //     }
+            //     else{
+            //         doc_id = 1000;
+            //     }
+            // })
+
+            db.collection("UserData").doc("Users").collection(user.email).doc("editor").collection("userEditor").doc(String(doc_id)).set({
+                title: editorTitle,
+                notes: outputData,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                doc_id: doc_id,
+            },
+                { merge: true })
+        }).catch((error) => {
+            console.log('Saving failed: ', error)
+        });
+
     }
 
     const editor = new EditorJS({
@@ -64,12 +64,15 @@ const Editor = () => {
         tools: {
             header: Header,
         },
-        onChange: () => saveData()
+        // onChange: () => saveData()
     })
 
     return (
         <>
             <h1>Editorrr</h1>
+            <div className={styles.editor_title}>
+                <input type="text" value={editorTitle} onChange={(e) => setEditorTitle(e.target.value)} placeholder="Enter Your Title" />
+            </div>
             <div className={styles.main_container}>
                 {/* <div className={styles.editor_title}>
                     <input type="text" value={editorTitle} onChange={(e) => setEditorTitle(e.target.value)} placeholder="Enter Your Title" />
---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>
+        <input type="text" />
+    </div>
+  )
+}
+
+export default Title
